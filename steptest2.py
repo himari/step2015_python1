3faa00810de793b57af2d5df3022865ff9db4194
himari <sunflowerhimari@gmail.com>
2015-06-04 20:34:55 +0900

    Update steptest2.py
    It is slow, or is not working. (but no errors)

--- steptest2.py
+++ steptest2.py
@@ -1,7 +1,6 @@
 import io
 import string
 
-#this first part will collect words from the user
 print "Enter 0 when you're done entering words.   "
 wordList = []
 t = True
@@ -12,18 +11,12 @@ while (t == True):
     else:
         t = False
 
-#this will make a hash table of words in the dictionary;
-#each entry will be an array of words that have the same set of letters,
-#and the key to each entry will be a sorted set of those letters.
 text = open("/usr/share/dict/words")
 dict_words = {}
 for i in text:
     #this sorts the letters in the word
     j = string.join(sorted(i.strip())).replace(' ', '')
-    #check to see if there is an entry for that sorted letter set;
-    #if there is, then append the current word to the array of other
-    #words that have those same letters; if not, make a new entry in
-    #the hash table and make the word the first entry in the new array.
+
     if j in dict_words:
         dict_words[j].append(i)
     else:
