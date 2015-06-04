14e13e71022d8d224440baddcf79882d3544a827
himari <sunflowerhimari@gmail.com>
2015-06-04 20:32:14 +0900

    Update steptest1.py
    Gives out a random word which is formed from 16 letters.

--- steptest1.py
+++ steptest1.py
@@ -9,14 +9,12 @@ def random_word( wlen ):
     flen = os.stat('/usr/share/dict/words').st_size
     chunks = flen / CHUNK_SIZE
     word = None
-    # We might have to try a couple times if we get a chunk that does
-    # not have any words of our requested length.
+    
     while not word:
         rpos = random.randint(0,chunks) * CHUNK_SIZE        
         wfile.seek(rpos)
         buf = wfile.read(CHUNK_SIZE)
-        # We skip the first and last word because the seeks and read
-        # may have left partial words on the ends.
+     
         words = [x  for x in buf.split("\n")[1:-1] if len(x) == wlen ]
         if words:
             p = random.randint(0,len(words)-1)
